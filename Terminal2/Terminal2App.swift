31949305e0ea0fc3e45362c94f30fa36f99040bc
mcantero15 <mariana.a.cantero15@gmail.com>
2023-08-05 17:16:48 -0400

    Initial Commit

---
+++ Terminal2/Terminal2App.swift
@@ -0,0 +1,17 @@
+//
+//  Terminal2App.swift
+//  Terminal2
+//
+//  Created by Mariana Cantero on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Terminal2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
